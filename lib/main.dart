b55ce6906d4d94e06e00feb196f20a816833add2
s-muel <samuel.essuman@amalitech.org>
2021-11-29 12:06:07 +0000

    added a card

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.brown,
-      ),
+      theme: ThemeData(primarySwatch: Colors.amber),
       home: const MyHomePage(),
     );
   }
@@ -46,30 +44,91 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Sam's Application"),
       ),
-      body: Center(
-          child: ListTile(
-        leading: const CircleAvatar(
-          radius: 30,
-          backgroundImage: AssetImage("assets/images/profile.jpg"),
-        ),
-        title: const Text("Solomon"),
-        subtitle: Row(
-          children: const [
-            Icon(Icons.done_all),
-            SizedBox(
-              width: 10,
+      body: Column(
+        children: [
+          ListTile(
+            leading: const CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage("assets/images/profile.jpg"),
             ),
-            Text("Hello send me the food")
-          ],
-        ),
-        trailing: Column(
-          children: const [Text("5:30"), Icon(Icons.notifications)],
-        ),
-      )),
+            title: const Text("Solomon"),
+            subtitle: Row(
+              children: const [
+                Icon(Icons.done_all),
+                SizedBox(
+                  width: 10,
+                ),
+                Text("Hello send me the food")
+              ],
+            ),
+            trailing: Column(
+              children: const [Text("5:30"), Icon(Icons.notifications)],
+            ),
+          ),
+          // this is my listTile
+          Card(
+            elevation: 10,
+            child: ListTile(
+              leading: const CircleAvatar(
+                radius: 30,
+                backgroundImage: AssetImage("assets/images/profile1.jpg"),
+              ),
+              title: const Text("Samuella Essuman"),
+              subtitle: Row(
+                children: const [
+                  Icon(Icons.done),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Please sir i am done"),
+                ],
+              ),
+              trailing: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: const [
+                    Text("11:30 AM"),
+                    CircleAvatar(
+                      radius: 10,
+                      child: Text("5"),
+                      backgroundColor: Colors.green,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
+class NewHome extends StatelessWidget {
+  const NewHome({Key? key}) : super(key: key);
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.black,
+              ),
+              title: Text("Samuel Essuma"),
+              subtitle: Row(
+                children: const [
+                  Icon(Icons.done_all),
+                  SizedBox(width: 10),
+                  Text("Hello Buddy ")
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
